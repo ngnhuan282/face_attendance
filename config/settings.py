6c5361b52bd72a5412cfb5c9c8e3bfea46e2c005
ngnhuan282 <nguyennhuan2003@gmail.com>
2026-05-14 23:15:25 +0700

    giao dien trang login va trang home

--- config/settings.py
+++ config/settings.py
@@ -58,7 +58,7 @@ ROOT_URLCONF = "config.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [BASE_DIR / 'templates'],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -122,4 +122,17 @@ MEDIA_ROOT = BASE_DIR / 'media'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = "static/"
+# ===================== STATIC FILES =====================
+STATIC_URL = '/static/'
+
+# Thư mục static ở mức project
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',           # 
+]
+
+# Sau khi đăng nhập, chuyển về trang chủ
+LOGIN_REDIRECT_URL = '/'
+LOGIN_URL = '/login/'
+
+# Default primary key field type
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
--- dashboards/views.py
+++ dashboards/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def home(request):
+    """Trang chủ giới thiệu hệ thống EduFace."""
+    return render(request, 'dashboards/home.html')
